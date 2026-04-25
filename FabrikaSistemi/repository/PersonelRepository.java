ac9584ac31d1302030fe2e1c6adb084a55b2241e
Elanur Tuana İŞCEN <elanurtuanaiscen@gmail.com>
2026-04-25 17:08:18 +0300

    Rename repository/PersonelRepository.java to FabrikaSistemi/repository/PersonelRepository.java

---
+++ FabrikaSistemi/repository/PersonelRepository.java
@@ -0,0 +1,11 @@
+package com.factory.stitch.repository;
+
+
+/**
+ * Bu sinif Fabrika ERP backend modulu icin dokumante edilmis Java bileşenidir.
+ */
+import com.factory.stitch.model.Personel;
+
+public class PersonelRepository extends BaseRepository<Personel> {
+}
+
