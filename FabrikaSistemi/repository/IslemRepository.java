594c6fb0c04a8ecf238fdeb5bc48cd6fdd1f5700
Elanur Tuana İŞCEN <elanurtuanaiscen@gmail.com>
2026-04-25 17:12:52 +0300

    Rename repository/IslemRepository.java to FabrikaSistemi/repository/IslemRepository.java

---
+++ FabrikaSistemi/repository/IslemRepository.java
@@ -0,0 +1,11 @@
+package com.factory.stitch.repository;
+
+
+/**
+ * Bu sinif Fabrika ERP backend modulu icin dokumante edilmis Java bileşenidir.
+ */
+import com.factory.stitch.model.Islem;
+
+public class IslemRepository extends BaseRepository<Islem> {
+}
+
